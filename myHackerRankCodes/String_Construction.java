c7513a1caa41e81bfc27ca1eca48035719e7e2ae
AJAY KUMAR <43679751+ajaykumar2017@users.noreply.github.com>
2020-02-26 00:49:41 +0530

    Files added
    Beautiful triplets and String Construction files added

---
+++ myHackerRankCodes/String_Construction.java
@@ -0,0 +1,28 @@
+package myHackerRankCodes;
+
+import java.util.*;
+
+public class String_Construction {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// input
+		int n = sc.nextInt(); // number of strings
+		for (int i = 0; i < n; i++) {
+			String str = sc.next();
+			int result = stringConstruction(str);
+			System.out.println(result);
+		}
+	}
+
+	private static int stringConstruction(String str) {
+		int cost = 0;
+		boolean[] status = new boolean[256];
+		for (int i=0; i<str.length(); i++) {
+			status[str.charAt(i)] = true;
+		}
+		for (int i=0; i<256; i++) {
+			if(status[i]) cost++;
+		}
+		return cost;
+	}
+}
