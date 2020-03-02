089b4bfdabf0b6cefabd65525ea8cb16318731ff
AJAY KUMAR <43679751+ajaykumar2017@users.noreply.github.com>
2020-03-03 01:03:40 +0530

    files added
    files added

---
+++ myHackerRankCodes/Mars_Exploration.java
@@ -0,0 +1,28 @@
+package myHackerRankCodes;
+
+import java.util.*;
+
+public class Mars_Exploration {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// input
+		String s = sc.next(); //the unencrypted string
+		//output
+		int result = marsExploration(s);
+		System.out.println(result);
+		
+	} 
+	
+	private static int marsExploration (String s){
+		int count = 0;
+		for(int i=0; i<(s.length()/3); i++) {
+			if(s.charAt(3*i)!='S')
+				count++;
+			if(s.charAt(3*i+1)!='O')
+				count++;
+			if(s.charAt(3*i+2)!='S')
+				count++;
+		}
+		return count;
+	}
+}
