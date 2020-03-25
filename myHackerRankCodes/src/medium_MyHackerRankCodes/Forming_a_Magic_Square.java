aa022514b2addb8ad9f0fc1d8658f93cd40cb09c
AJAY KUMAR <43679751+ajaykumar2017@users.noreply.github.com>
2020-03-25 20:26:50 +0530

    Medium myHackerRank Codes Added
    Medium myHackerRank Codes Added

--- myHackerRankCodes/src/medium_MyHackerRankCodes/Forming_a_Magic_Square.java
+++ myHackerRankCodes/src/medium_MyHackerRankCodes/Forming_a_Magic_Square.java
@@ -3,7 +3,6 @@ package medium_MyHackerRankCodes;
 import java.util.Scanner;
 
 public class Forming_a_Magic_Square {
-
 	private static final Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
 		//Taking input
@@ -17,7 +16,41 @@ public class Forming_a_Magic_Square {
 		//Showing output
 		System.out.println(result);
 	}
-    private static int formingMagicSquare(int[][] matrix) {
-    	return 0;
-    }
+	
+	private static int formingMagicSquare(int[][] matrix) {
+	    int[][][] d = {
+	    	{{8,1,6},{3,5,7},{4,9,2}},
+		    {{6,1,8},{7,5,3},{2,9,4}},
+		    {{4,9,2},{3,5,7},{8,1,6}},
+		    {{2,9,4},{7,5,3},{6,1,8}},
+		    {{8,3,4},{1,5,9},{6,7,2}},
+		    {{4,3,8},{9,5,1},{2,7,6}},
+		    {{6,7,2},{1,5,9},{8,3,4}},
+		    {{2,7,6},{9,5,1},{4,3,8}}};
+		int c[] = new int[8];
+		int min1;
+		for(int k=0;k<8;k++)
+		{
+			c[k] = 0;
+	      for(int i=0;i<3;i++)
+		  {
+		    for(int j=0;j<3;j++)
+		    {
+		    	if(matrix[i][j]!=d[k][i][j])
+		    	{
+		    		c[k]+=Math.abs(matrix[i][j]-d[k][i][j]);
+
+		    	}
+	 	    }
+		  }
+		}
+		min1 = c[0];
+		for(int i=1;i<8;i++){
+			if(c[i]<min1)
+		  	{
+		  		min1=c[i];
+			}
+		}
+	    return min1;
+	}
 }
