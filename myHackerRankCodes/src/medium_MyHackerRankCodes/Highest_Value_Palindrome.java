a184ed7375112d74a7b11ea13ebdf315cd0b1929
AJAY KUMAR <43679751+ajaykumar2017@users.noreply.github.com>
2020-05-30 01:46:47 +0530

    Add files via upload

--- myHackerRankCodes/src/medium_MyHackerRankCodes/Highest_Value_Palindrome.java
+++ myHackerRankCodes/src/medium_MyHackerRankCodes/Highest_Value_Palindrome.java
@@ -1,5 +1,22 @@
 package medium_MyHackerRankCodes;
 
-public class Highest_Value_Palindrome {
+import java.util.Scanner;
 
+public class Highest_Value_Palindrome {
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// Taking input from user
+		int n = scanner.nextInt(); // The number of digits in the number
+		int k = scanner.nextInt(); //maximum number of changes allowed
+		String s = scanner.next(); // n digit string of numbers
+
+		// Showing the output
+		String result = highestValuePalindrome(s);
+		System.out.println(result);
+	}
+	
+	private static String highestValuePalindrome(String s, int n, int k) {
+		
+    }
 }
