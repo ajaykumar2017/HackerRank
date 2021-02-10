69dfddf8db82ea0a397ed93388bce24f4d9ec903
AJAY KUMAR <43679751+ajaykumar2017@users.noreply.github.com>
2021-02-11 02:36:38 +0530

    TCS Wings1 solution
    TCS Wings1 solution

--- Practice/src/TCS_Wings1_Q1.java
+++ Practice/src/TCS_Wings1_Q1.java
@@ -13,46 +13,46 @@ public class TCS_Wings1_Q1 {
 		}
 
 		// Printing Output
-		System.out.println(maxProfit(arr, n));
+		System.out.println(maxBenefit(arr, n));
 
 	}
 
-	private static int maxProfit(int[] arr, int n) {
-		List<Integer> profitList = new ArrayList<Integer>();
-		// Initialize first element of array to profit
-		int profit = arr[0];
+	private static int maxBenefit(int[] arr, int n) {
+		List<Integer> benefitList = new ArrayList<Integer>();
+		// Initialize first element of array to benefit
+		int benefit = arr[0];
 
 		// Iterate from 1 to n-1 to compare the first element to next one
 		for (int i = 1; i < n; i++) {
 			/**
-			 * Checking if current element is bigger than previous one then add to profit as
+			 * Checking if current element is bigger than previous one then add to benefit as
 			 * we have to find the sorted subarray with highest sum of elements
 			 */
 			if (arr[i] > arr[i - 1]) {
-				profit = profit + arr[i];
+				benefit = benefit + arr[i];
 			} else {
 				/*
-				 * add to profitList if current element is smaller than previous current element
+				 * add to benefitList if current element is smaller than previous current element
 				 * will be part of next subarray
 				 */
-				profitList.add(profit);
-				// Assign current element to profit as this is the part of next subarray
-				profit = arr[i];
+				benefitList.add(benefit);
+				// Assign current element to benefit as this is the part of next subarray
+				benefit = arr[i];
 			}
 			/**
-			 * Check if it is the last element of the array so we have to add to profitList
+			 * Check if it is the last element of the array so we have to add to benefitList
 			 * as no more comparison
 			 */
 			if (i == n - 1)
-				profitList.add(profit);
+				benefitList.add(benefit);
 
 		}
 
-		// Sort the profitList as we need to get the greatest element
-		Collections.sort(profitList, Collections.reverseOrder());
+		// Sort the benefitList as we need to get the greatest element
+		Collections.sort(benefitList, Collections.reverseOrder());
 
 		// Return the first element as it is the greatest element of the list
-		return profitList.get(0);
+		return benefitList.get(0);
 	}
 
 }
