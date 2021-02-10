3f9b69dff0aac2ab6c3167130f571975dd0315ee
AJAY KUMAR <43679751+ajaykumar2017@users.noreply.github.com>
2021-02-11 01:40:03 +0530

    Add files via upload

--- Practice/src/TCS_Wings1_Q1.java
+++ Practice/src/TCS_Wings1_Q1.java
@@ -1,33 +1,57 @@
 import java.util.*;
+
 public class TCS_Wings1_Q1 {
 
 	public static final Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		// Taking the input from user
 		int n = sc.nextInt();
 		int[] arr = new int[n];
-		for(int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
-		
+
+		// Printing Output
 		System.out.println(maxProfit(arr, n));
 
 	}
-	
+
 	private static int maxProfit(int[] arr, int n) {
 		List<Integer> profitList = new ArrayList<Integer>();
+		// Initialize first element of array to profit
 		int profit = arr[0];
+
+		// Iterate from 1 to n-1 to compare the first element to next one
 		for (int i = 1; i < n; i++) {
-			if(arr[i] > arr[i - 1]) {
+			/**
+			 * Checking if current element is bigger than previous one then add to profit as
+			 * we have to find the sorted subarray with highest sum of elements
+			 */
+			if (arr[i] > arr[i - 1]) {
 				profit = profit + arr[i];
 			} else {
+				/*
+				 * add to profitList if current element is smaller than previous current element
+				 * will be part of next subarray
+				 */
 				profitList.add(profit);
+				// Assign current element to profit as this is the part of next subarray
 				profit = arr[i];
 			}
-			if(i == n - 1) profitList.add(profit);
-				
+			/**
+			 * Check if it is the last element of the array so we have to add to profitList
+			 * as no more comparison
+			 */
+			if (i == n - 1)
+				profitList.add(profit);
+
 		}
+
+		// Sort the profitList as we need to get the greatest element
 		Collections.sort(profitList, Collections.reverseOrder());
+
+		// Return the first element as it is the greatest element of the list
 		return profitList.get(0);
 	}
 
